c9f4adccad1a5b52828d7101b8d2413555490385
Curtis Mckee <cmckee.dev@gmail.com>
2020-06-11 19:18:58 -0600

    Initial Commit - Personal bash script on request

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+FEED = "https://feeds.feedwrench.com/SustainOurSoftware.rss"
+
+for item in $feed; do
+
+  # Get <title> and parse out title
+  
+  # make metadata file
+
+  # Write to metadata file
+    # epId <title> Parse out SOS 013
+    # title <title> Parse out title
+    # pubDate <pubDate>
+    # duration <itunes:duration>
+    # Description <Description>
+
+  # Download mp3 <enclosure>
+
+done
+
